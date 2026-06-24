3fa878d962548ec27d2defc8995bf5ebf7b8434d
VELASQUE DURAND, JHON CARLOS JEAN PAUL <jhon.velasque@unmsm.edu.pe>
2026-06-23 22:34:43 -0500

    MI INICIO DE MI PROYECTO
    - CREE MI DDL

---
+++ Scripts/ddl.sql
@@ -0,0 +1,52 @@
+-- Creando nuestra base de datos 
+CREATE DATABASE BD_HR_3RA ;
+GO
+
+USE BD_HR_3RA ;
+GO
+
+---------------------------------
+-- CARGA EMPLEADOS
+---------------------------------
+IF OBJECT_ID('Employee', 'U') IS NOT NULL
+    DROP TABLE Employee;
+-- Creando la tabla Employee para importar Employee.csv
+CREATE TABLE dbo.Employee (
+    EmployeeID varchar(20) NOT NULL PRIMARY KEY,
+    FirstName varchar(50) NULL,
+    LastName varchar(50) NULL,
+    Gender varchar(20) NULL,
+    Age INT NULL,
+    BusinessTravel varchar(30) NULL,
+    Department varchar(50) NULL,
+    DistanceFromHome_KM tinyint NULL,
+    State char(2) NULL,
+    Ethnicity varchar(80) NULL,
+    Education tinyint NULL,
+    EducationField varchar(60) NULL,
+    JobRole varchar(80) NULL,
+    MaritalStatus varchar(20) NULL,
+    Salary int NULL,
+    StockOptionLevel tinyint NULL,
+    OverTime varchar(5) NULL,
+    HireDate date NULL,
+    Attrition varchar(3) NULL,
+    YearsAtCompany tinyint NULL,
+    YearsInMostRecentRole tinyint NULL,
+    YearsSinceLastPromotion tinyint NULL,
+    YearsWithCurrManager tinyint NULL
+);
+
+-- IMPORTANDO DATOS DE CSV
+BULK INSERT Employee
+FROM 'C:\HOME LOCAL\SQL-SERVER-HR-ANALYTTCS-3RAEDICION\Data\Employee.csv' -- Reemplaza con la ruta real
+WITH (
+    FORMAT = 'CSV',
+    FIRSTROW = 2,           -- Ignora el encabezado del CSV
+    FIELDTERMINATOR = ',',  -- Cambia por ';' si tu CSV usa punto y coma
+    ROWTERMINATOR = '\n' -- Salto de línea estándar (LF) o '\n'
+   -- ENCODING = 'UTF-8'      -- Importante si tienes acentos o tildes
+ 
+);
+
+
